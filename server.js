3044c60e6b516957ac19c1f24ccc6879f8590ac6
Vamshi <vamshiediga2003@gmail.com>
2025-06-18 19:40:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '10mb' }));
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, "MongoDB connection error"));
 db.once('open', () => console.log("Connected to Database"));
