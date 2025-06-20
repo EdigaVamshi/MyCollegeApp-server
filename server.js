9ddd8c8c20a4cf2ce878daaa40ddf63927fd8eaf
Vamshi <vamshiediga2003@gmail.com>
2025-06-20 13:18:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -35,6 +35,11 @@ app.get('/api/colleges-info', async (req, res) => {
         console.log(err);
         res.status(500).json({ error: "Failed to retrieve colleges-info" });
     }
-})
+});
+
+app.get('/ping', (req, res) => {
+  res.status(200).send('pong');
+});
+
 
 app.listen(process.env.PORT, () => console.log(`Server running on port ${process.env.PORT}`));
